93531bc0634d03ba2f2f645637ab0ac95a06978f
Prayoch Rujira <prayoch@clockupstudio.com>
2021-08-28 18:16:39 +0700

    Connected to the database

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,24 @@
+from meetups.models import Meetup
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = Meetup.objects.all()
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
+
+def meetup_details(request, meetup_slug):
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Exception as e:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })
