f6c979143b4fa299a3cb0bebc89fdc41d5765944
Prayoch Rujira <prayoch@clockupstudio.com>
2021-08-30 18:29:13 +0700

    Added registration form

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Location, Meetup
+from .models import Location, Meetup, Participant
 
 # Register your models here.
 admin.site.register(Meetup)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,6 @@
-from meetups.models import Meetup
-from django.shortcuts import render
+from meetups.forms import RegistrationForm
+from meetups.models import Meetup, Participant
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 
@@ -13,12 +14,26 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+
+        if request.method == 'GET':    
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, _ = Participant.objects.get_or_create(email=user_email)
+                selected_meetup.participants.add(participant)
+                return redirect('confirm-registration')
+        
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description': selected_meetup.description
-        })
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as e:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
+
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
