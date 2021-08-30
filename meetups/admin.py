e04d9a6ccba033ca1f5b2d0942d5711965269423
Prayoch rujira <prayoch@clockupstudio.com>
2021-08-30 11:03:56 +0700

    Supported images

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import Location, Meetup
 
 # Register your models here.
 admin.site.register(Meetup)
+admin.site.register(Location)
